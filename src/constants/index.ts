6e4ce2f39cce1032c836be1ca7e379a684fc3bd8
andreas <hoang.dao@inbox4us.xyz>
2024-04-04 12:52:47 +0700

    feat(create org): force create org if user is not in any org - fix constant error

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,14 +4,12 @@ import {
     CONTACT_BOOK,
     HOME_PATH,
     PAYMENT_METHOD,
-    PROFILE_PATH,
     SIGN_IN_PATH,
     SIGN_OUT_PATH
 } from "@/constants/routingPath";
 
 module.exports = {
     SideBarData,
-    PROFILE_PATH,
     HOME_PATH,
     SIGN_IN_PATH,
     SIGN_OUT_PATH,
