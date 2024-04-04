9d80b4d829aa4d8a15051efad3eae26b0739f6fb
andreas <hoang.dao@inbox4us.xyz>
2024-04-04 14:23:42 +0700

    feat(create org): force create org if user is not in any org - fix middlewares

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,12 +17,47 @@ export default authMiddleware({
     afterAuth(auth, req, evt) {
         // Handle users who aren't authenticated
         if (!auth.userId && !auth.isPublicRoute && req.nextUrl.pathname != SIGN_IN_PATH) {
+            console.log("!auth.userId && !auth.isPublicRoute && req.nextUrl.pathname != SIGN_IN_PATH", SIGN_IN_PATH)
             return redirectToSignIn({returnBackUrl: req.url});
         }
 
-        // if (auth.userId && !auth.isPublicRoute) {
-        //     if (auth.sessionClaims != null && req.nextUrl.pathname != CREATE_ORGANIZATION_PATH) {
-        //         console.log("auth.sessionClaims != null && req.nextUrl.pathname != CREATE_ORGANIZATION_PATH")
+        if (auth.userId && !auth.isPublicRoute) {
+            console.log("auth.userId && !auth.isPublicRoute")
+
+            if (auth.sessionClaims != null && req.nextUrl.pathname != CREATE_ORGANIZATION_PATH) {
+                console.log("auth.sessionClaims != null && req.nextUrl.pathname != CREATE_ORGANIZATION_PATH")
+                const memberships = auth.sessionClaims.memberships;
+                if (Object.keys(memberships).length == 0) {
+                    console.log("Object.keys(memberships).length == 0")
+                    return NextResponse.redirect(new URL(CREATE_ORGANIZATION_PATH, req.url))
+                }
+                // return NextResponse.redirect(new URL(CREATE_ORGANIZATION_PATH, req.url))
+            } else {
+                if (
+                    auth.userId &&
+                    !auth.orgId &&
+                    req.nextUrl.pathname !== SELECT_ORGANIZATION_PATH
+                ) {
+                    const orgSelection = new URL(SELECT_ORGANIZATION_PATH, req.url);
+                    return NextResponse.redirect(orgSelection);
+                }
+
+            }
+        }
+
+        // If the user is signed in and trying to access a protected route, allow them to access route
+        if (auth.userId && !auth.isPublicRoute) {
+            if (req.nextUrl.pathname == HOME_PATH) {
+                return NextResponse.redirect(new URL(DASHBOARD_PATH, req.url))
+            }
+        }
+
+        // // Redirect signed in users to organization selection page if they are not active in an organization
+        // else if (auth.sessionClaims != null && req.nextUrl.pathname != CREATE_ORGANIZATION_PATH) {
+        //     console.log("auth.sessionClaims != null && req.nextUrl.pathname != CREATE_ORGANIZATION_PATH")
+        //     const memberships = auth.sessionClaims.memberships;
+        //     if (Object.keys(memberships).length == 0) {
+        //         console.log("Object.keys(memberships).length == 0")
         //         return NextResponse.redirect(new URL(CREATE_ORGANIZATION_PATH, req.url))
         //     } else {
         //         // if (
@@ -43,33 +78,8 @@ export default authMiddleware({
         //
         //     }
         // }
-        // Redirect signed in users to organization selection page if they are not active in an organization
-        else if (auth.sessionClaims != null && req.nextUrl.pathname != CREATE_ORGANIZATION_PATH) {
-            console.log("auth.sessionClaims != null && req.nextUrl.pathname != CREATE_ORGANIZATION_PATH")
-            const memberships = auth.sessionClaims.memberships;
-            if (Object.keys(memberships).length == 0) {
-                console.log("Object.keys(memberships).length == 0")
-                return NextResponse.redirect(new URL(CREATE_ORGANIZATION_PATH, req.url))
-            } else {
-                if (
-                    auth.userId &&
-                    !auth.orgId &&
-                    req.nextUrl.pathname !== SELECT_ORGANIZATION_PATH
-                ) {
 
-                    const orgSelection = new URL(SELECT_ORGANIZATION_PATH, req.url);
-                    return NextResponse.redirect(orgSelection);
-                }
-                // If the user is signed in and trying to access a protected route, allow them to access route
-                if (auth.userId && !auth.isPublicRoute) {
-                    if (req.nextUrl.pathname == HOME_PATH) {
-                        return NextResponse.redirect(new URL(DASHBOARD_PATH, req.url))
-                    }
-                }
-
-            }
-        }
-
+        console.log("ELSE: ", req.nextUrl.pathname)
         // Allow users visiting public routes to access them
         return NextResponse.next();
     },
