0727d4a24c682d9393540ce65de937aad48e401f
andreas <hoang.dao@inbox4us.xyz>
2024-04-08 21:43:09 +0700

    fix bug not show org photo, name

--- src/app/general-checkout-page/[orgId]/page.tsx
+++ src/app/general-checkout-page/[orgId]/page.tsx
@@ -1,13 +1,15 @@
 import { getActivePaymentMethod } from "@/lib/actions/paymentMethodAction";
-import { OrgInformation } from "./org-image"
 import { Separator } from "@/components/ui/separator"
 import { PaymentSchedule } from "@/lib/model/enum";
 import PaymentMethodCardReadonly from "./payment-method";
 import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { clerkClient } from "@clerk/nextjs";
 
 
 export default async function GeneralCheckoutPage({ params }: { params: { orgId: string } }) {
 
+  let organization = await clerkClient.organizations.getOrganization({ organizationId: params.orgId! })
   let data = await getActivePaymentMethod({ isActive: true, orgId: params.orgId });
   let paynow = data.data?.filter((pm) => pm.schedule === PaymentSchedule.NOW).sort((a, b) => a.id! - b.id!)
   let payLater = data.data?.filter((pm) => pm.schedule === PaymentSchedule.LATER).sort((a, b) => a.id! - b.id!)
@@ -16,7 +18,18 @@ export default async function GeneralCheckoutPage({ params }: { params: { orgId:
     <div className="flex justify-center  w-full h-screen mt-20">
 
       <div className="w-[750px] space-y-8">
-        <OrgInformation />
+        <div className="flex items-center gap-8">
+          <Avatar className="w-24 h-24">
+            <AvatarImage width={200} height={200} src={organization?.imageUrl} />
+            <AvatarFallback>{organization?.name.at(0)}</AvatarFallback>
+          </Avatar>
+
+          <div className="text-xl font-semibold tracking-tight">
+            {organization?.name}
+          </div>
+
+        </div>
+
 
         <div className="flex flex-col gap-8">
           <h2 className="text-xl font-bold tracking-tight">Available payment options</h2>
